11bfe3541b9cbfd9a3e5cab6a59f7037e428ffa9
gabe <menendez.gabriel@gmail.com>
2020-09-06 20:27:16 -0300

    WIP: Add files for employee
    - fix url
    - change home by index
    - add side-nav component

--- vmSystem/apps/employees/urls.py
+++ vmSystem/apps/employees/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import EmployeePage
 
 urlpatterns = [
-    path("", EmployeePage.as_view(), name="employee"),
+    path("", EmployeePage.as_view(), name="index"),
 ]
--- vmSystem/apps/employees/views.py
+++ vmSystem/apps/employees/views.py
@@ -10,6 +10,6 @@ class EmployeePage(View):
     """
 
     def get(self, request, *args, **kwargs):
-        template_name = "employees/home.html"
+        template_name = "employees/index.html"
         # contex_object = "employee"
         return render(request, template_name)
